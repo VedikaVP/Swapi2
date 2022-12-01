a787ecb5dcf0bbc6d22e52cfe173b0e3d6cad9ae
Vedika_B <vedika.patange@gmail.com>
2022-12-01 13:09:56 +0530

    commited film code

--- Caller.py
+++ Caller.py
@@ -1,21 +1,33 @@
 from Resources.People import People
 from Resources.Vehicle import Vehicle
+from Resources.Films import Films
+
 
 # Methods to fetch Data
-def getPeopleData():
+def get_people_data():
     obj = People()
     obj.fetch()
     obj.get_names()
     obj.get_count()
 
-def getVehicleData():
+
+def get_vehicle_data():
     obj = Vehicle()
     obj.fetch()
     obj.get_names()
     obj.get_count()
 
 
+def get_film_data():
+    obj = Films()
+    obj.fetch()
+    obj.get_names()
+    obj.get_count()
+
+
 # Fetch People Data
-getPeopleData()
+# getPeopleData()
 # Fetch Vehicle Data
-getVehicleData()
+# getVehicleData()
+# fetch films data
+get_film_data()
---
+++ Resources/Films.py
@@ -0,0 +1,45 @@
+from Service.MainService import Service
+from Resources.ResourceBase import Base
+
+
+class Films(Base):
+    # Variables
+    __response = []
+    __page: int = 1
+    __serviceObj = Service()
+
+    # Initializers
+    def __init__(self, service=Service()):
+        super().__init__()
+        self.__serviceObj = service
+
+    # URL related code block
+    def __get_relative_url(self):
+        return "films/"
+
+    def __get_complete_url(self):
+        return self.host_url + self.__get_relative_url()
+
+    # Data fetch related part
+    def fetch(self):
+        params = {"page": self.__page}
+        res = self.__serviceObj.get_response(self.__get_complete_url(), params)
+        results_data = res.get("results", [])
+        count = res.get("count", 0)
+        self.__response.extend(results_data)
+        if len(self.__response) < int(count):
+            self.__page += 1
+            self.fetch()
+
+    def get_names(self):
+        if len(self.__response) > 0:
+            film_names = list(map(lambda x: x.get("title"), self.__response))
+            print(film_names)
+        else:
+            print("No film name data found")
+
+    def get_count(self):
+        if len(self.__response) > 0:
+            print("{0} data found".format(len(self.__response)))
+        else:
+            print("No film {0} data found".format(len(self.__response)))
